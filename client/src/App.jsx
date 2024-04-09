ac98543c1f36e6e6144e6f8ce0a5bb4ad058d708
Festus <techarenaz01@gmail.com>
2024-04-08 17:04:05 -0700

    Create pages and route for the blog

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Dashboard from "./pages/Dashboard"
+import PrivacyPolicy from "./pages/PrivacyPolicy"
 
 function App() {
   return (
     <div>
-      <h1 className=''>Tech Arena24</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home />} />
+        <Route path="about" element = {<About />} />
+        <Route path="contact" element = {<Contact />} />
+        <Route path="dashboard" element = {<Dashboard />} />
+        <Route path="privacy-policy" element = {<PrivacyPolicy />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
